1f99a19886428f943e07b33ffcf3f6e55f0ab32d
Stephen M. Senesac <stephen.senesac@ethereal-soft.com>
2018-06-02 19:07:40 -0400

    Fix build script for multiple test projects.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,4 @@ set -e
 dotnet clean src/Ethereal/Ethereal.sln -c Release
 dotnet restore src/Ethereal/Ethereal.sln
 dotnet build src/Ethereal/Ethereal.sln -c Release --no-restore
-dotnet test src/Ethereal/**/*.Test.csproj -c Release --no-build --no-restore
+ls src/Ethereal/**/*.Test.csproj | xargs -L1 dotnet test -c Release --no-build --no-restore
