0dce30611c84eb83fc15417183a4ea1e198dc3bb
Stephen M. Senesac <stephen.senesac@ethereal-soft.com>
2018-07-31 23:16:22 -0400

    Fix build.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,5 @@ set -e
 cd src/Ethereal
 
 dotnet restore
-dotnet build -c release --no-restore
-ls **/*.Test.csproj | xargs -L1 dotnet test -c release --no-build --no-restore
+dotnet build -c Release --no-restore
+ls **/*.Test.csproj | xargs -L1 dotnet test -c Release --no-build --no-restore
